590e8f645e927a596ffc0335431dd40d4190a316
Grecy Tembhare <tembharegrecy@gmail.com>
2024-04-25 19:29:28 +0530

    feat: updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,29 @@ import { Routes, Route } from "react-router-dom";
 import LoginForm from "./components/LoginSignUp/LoginForm";
 import Register from "./components/LoginSignUp/Register";
 import Dashboard from "./pages/Dashboard";
-// import { List } from "@material-tailwind/react";
-import List from "./components/Board/List"
-import Board from "./components/Board";
-// import Analytics from "./pages/Analytics";
-import Body from "./components/Board/Body";
+import Board from "./components/BoardContents";
 import Analytics from "./pages/Analytics";
-
-
-
-
+import Body from "./components/Board/Body";
+import WorkList from "./components/Dashboard2/List";
 
 function App() {
- 
   return (
     <>
       <Routes>
-        <Route path="/login" element={<LoginForm/>} />
-        <Route path="/signup" element={<Register/>} />
-        <Route path="/body" element={<Body />}/>
-        <Route path="/dashboard" element={<Dashboard />} >
-          <Route path="board" element={<Board/>} />
-          <Route path="analytics" element={<Analytics/>} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="/body" element={<Body />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="board">	
+            <Route index element={<Board />} /> 
+            <Route path="todoboards/:boardId" element={<WorkList />} />
+          </Route>
+          <Route path="analytics" element={<Analytics />} />
         </Route>
+        <Route path="*" element={<div>No match</div>} />
       </Routes>
-
-
-      
     </>
-  );
+ );
 }
-
-
 
 export default App;
